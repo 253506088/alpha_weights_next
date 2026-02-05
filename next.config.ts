ae09a54eb1d5749e90d5aa9308a366c361d39e03
blackTv-RedmiBook2024 <2842513372@qq.com>
2026-02-05 14:02:14 +0800

    配置

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    output: 'export',      // 启用静态导出
+    images: { unoptimized: true },  // 禁用图片优化
 };
 
 export default nextConfig;
