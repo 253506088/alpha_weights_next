534dacd7808c4839fe77a6bc0b350cd3abdd13ec
blackTv-RedmiBook2024 <2842513372@qq.com>
2026-02-05 14:50:55 +0800

    github pages

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,13 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === 'production';
+const repo = 'alpha_weights_next';
+
 const nextConfig: NextConfig = {
     output: 'export',      // 启用静态导出
     images: { unoptimized: true },  // 禁用图片优化
+    basePath: isProd ? `/${repo}` : '',
+    assetPrefix: isProd ? `/${repo}/` : '',
 };
 
 export default nextConfig;
